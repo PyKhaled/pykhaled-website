7a92a32fe68d237185a1d4438eb4ed2c8e12e836
Ahmed Khaled <ahmedaka@icloud.com>
2017-03-14 12:54:09 +0200

    clean up the code

--- js/main.js
+++ js/main.js
@@ -1,23 +1,13 @@
 $(function(){
 
-	// make banner image shows up slowlly
-	$(".banner__img").slideUp(100).delay(350).fadeIn(600);
-
 	// hide loading image after loads
 	$(window).load(function(){
 		$("#loader").hide();
 	});
 
+
+
+
+
+
 });
-
-
-
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("mainNav").style.width = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("mainNav").style.width = "0%";
-}
